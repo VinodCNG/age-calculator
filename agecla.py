386b2e60249f4681331a952560e613bc2e9f8aec
vinod <119941159+VinodCNG@users.noreply.github.com>
2022-12-06 20:59:52 +0530

    Add files via upload

---
+++ agecla.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from datetime import date
+
+root=Tk()
+root.geometry("800x600")
+root.resizable(False,False)
+root.title("Age calculator")
+
+def y():
+    today=date.today()
+    birthDate=date(int(yearEntry.get()),int(monthEntry.get()),int(dayEntry.get()))
+    age=today.year - birthDate.year-((today.month,today.day)<(birthDate.month,birthDate.day))
+    Label(text=f"{nameValue.get()} your age is {age}",font=30).place(x=400,y=500)
+
+photo=PhotoImage(file="Age calculator  .png")
+myimage=Label(image=photo)
+myimage.pack(padx=15,pady=15)
+
+Label(text="Name",font=23).place(x=200,y=250)
+Label(text="Year",font=23).place(x=200,y=300)
+Label(text="Month",font=23).place(x=200,y=350)
+Label(text="Day",font=23).place(x=200,y=400)
+
+nameValue=StringVar()
+yearValue=StringVar()
+monthValue=StringVar()
+dayValue=StringVar()
+
+nameEntry=Entry(root,x=nameValue,width=30,bd=3,font=20)
+nameEntry.place(x=300,y=250)
+
+yearEntry=Entry(root,x=yearValue,width=30,bd=3,font=20)
+yearEntry.place(x=300,y=300)
+
+monthEntry=Entry(root,x=monthValue,width=30,bd=3,font=20)
+monthEntry.place(x=300,y=350)
+
+dayEntry=Entry(root,x=dayValue,width=30,bd=3,font=20)
+dayEntry.place(x=300,y=400)
+
+Button(text="Cslculator",font=20,bg="black",fg="white",width=11,height=2,command=y).place(x=400,y=450)
+
+root.mainloop()
